631444a85e716645763f74b4a7fb1477ad61c589
Unknown <lustiger.luke@gmail.com>
2017-12-21 23:46:11 +0100

    followed idea
    looks pretty good. in this way we will get number and size of particles

--- main.m
+++ main.m
@@ -14,32 +14,41 @@ for k = 1 : NF
  
 end
 
-I=images{1}
+I=images{1};
 I=rgb2gray(I);
 level = graythresh(I); %get the perfect threshold from this Image ;)
 BW = im2bw(I,level);
 
 
-[labels,num] = bwlabel(BW) %Find connected areas and label them with numbers
-disp(num)
-RGB = label2rgb(labels)
-figure('name','labels')    
-imshow(RGB)
+[labels,num1] = bwlabel(BW); %Find connected areas and label them with numbers
+RGB = label2rgb(labels,'gray'); %gray defines the colormap. it is int (1:256)
+% figure('name','RGB image')    
+% imshow(RGB)
 
-se=strel('disk',10);  
-BW=imerode(~BW,se);
-figure('name','blackwhite2');
-imshow(~BW)
+vec = 1:256;
+[tf,loc] = ismember(vec,RGB);
+
+num2=loc(loc~=0);           %get a vector filled with object sizes
+number_of_objects = length(num2)
+mean_object_size = mean(num2)
+
+
+
+% se=strel('disk',10);  
+% BW=imerode(~BW,se);
+% figure('name','blackwhite2');
+% imshow(~BW)
 
 %L = watershed(images{1})
 %STATS = regionprops(L, 'Area')
 %[A,n] = findPartikel(inputPic)
 
+
+% figure('name','RESULT');
+% subplot(2,1,1);
+% imagesc(inputPic);
+% title('input');
+% subplot(2,1,2); 
+% imagesc(resizedPic);
+% title('watershed');
 disp('SUCCESS')
-figure('name','RESULT');
-subplot(2,1,1);
-imagesc(inputPic);
-title('input');
-subplot(2,1,2); 
-imagesc(resizedPic);
-title('watershed');
